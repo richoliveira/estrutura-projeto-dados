338236c574c567988832887ebe5c3fa03545946e
richoliveira <wallacerafael01@gmail.com>
2023-12-27 14:59:16 -0300

    continuous integration versao 0.3.3

--- exec/test-pipeline-batch-absenteism.sh
+++ exec/test-pipeline-batch-absenteism.sh
@@ -3,13 +3,15 @@
 # POPULA VARIAVEIS NECESSARIAS PARA A PIPELINE
 export PATH=/opt/poetry/bin:$PATH
 export PYTHONPATH=.:$PYTHONPATH
+export path_project=$PWD
+export path_github_action="/home/runner/work/estrutura-projeto-dados"
+export path_github_action_bin="/home/runner/.cache/pypoetry/virtualenvs/pacote-estrutura-projeto-NxwJ7pYs-py3.12/bin"
 
-# EXECUTA OS TESTE UNITARIOS
-poetry --version
+# VERIFICA SE O DIRETORIO NO PROJETO GITHUB ACTION
+if [[ "$path_project" == *"$path_github_action"* ]]; then
+    export PATH=$path_github_action_bin:$PATH
 
-ls /home/runner/.cache/pypoetry/virtualenvs/pacote-estrutura-projeto-NxwJ7pYs-py3.12
-
-
-ls /home/runner/.cache/pypoetry/virtualenvs/pacote-estrutura-projeto-NxwJ7pYs-py3.12/bin
-
-echo $PWD
+    pytest -v
+else
+    pytest -v
+fi
