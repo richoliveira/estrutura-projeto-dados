2de2849ecf9ded8e7ddb990c68b2a436d525feed
richoliveira <wallacerafael01@gmail.com>
2023-12-27 13:19:14 -0300

    continuous integration versao 0.2.4 teste

--- exec/test-pipeline-batch-absenteism.sh
+++ exec/test-pipeline-batch-absenteism.sh
@@ -5,8 +5,5 @@ export PATH=/opt/poetry/bin:$PATH
 export PYTHONPATH=.:$PYTHONPATH
 export path_project="/data/projetos/estrutura-projeto-dados/"
 
-#ABILITA VENV
-poetry shell
-
 # EXECUTA OS TESTES
-pytest -v
+pwd
